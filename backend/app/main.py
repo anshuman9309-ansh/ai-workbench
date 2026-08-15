35337d8b47157fba4f8cfbb0c72b3f52c85737e5
Anshuman <anshuman9309@gmail.com>
2026-08-15 17:01:57 +0530

    Modifed main.py for CORS and updated the frontend port to 8005 in the CORS setting. Update the yml file to map 8005 port to 80

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8000", "http://127.0.0.1:8000", "null"],
+    allow_origins=["http://localhost:8005", "http://127.0.0.1:8005", "http://localhost:8000", "http://127.0.0.1:8000", "null"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
